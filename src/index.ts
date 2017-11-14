ff1f7aa9cfd490656859bdab5f7a361b2a4e1284
McDonough <h273859@ga69mln00029c.honeywell.com>
2017-11-14 13:51:37 -0500

    testing multi-instance

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import express = require("express");
+import crypto = require("crypto");
 
 let app = express();
 let IP_ADDR = process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0";
+let instanceId = crypto.randomBytes(8).toString("hex");
 
 let PORT = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 app.all("/**",(req, res) => {
-    console.log(`received request from ${req.ip} to ${req.url}`);
+    console.log(`${instanceId}: received request from ${req.ip} to ${req.url}`);
     res.write(`req.url:${req.url}\n`);
     req.pipe(res);
 })
 
 app.listen(PORT, IP_ADDR, () => {
-    console.log(`Server listening on port ${PORT} for host ${IP_ADDR}`);
+    console.log(`${instanceId}: Server listening on port ${PORT} for host ${IP_ADDR}`);
 })
